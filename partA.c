f04527e2e92bcb7d4bf307cc283cbd69cc5a2507
Bruno <brunoazizsm@gmail.com>
2024-11-01 17:54:17 -0300

    nao funciona nada, tem q arruma funcao bsearch_lower_bound_Parallel

--- partA.c
+++ partA.c
@@ -7,6 +7,44 @@
 
 pthread_barrier_t mybarrier;
 
+typedef struct {
+    long long *array;
+    int n;
+    long long value;
+    int *Pos;
+    int index;
+} ParametroBusca;
+
+void* bsearch_lower_bound_Parallel(void *arg){
+    ParametroBusca *dados = (ParametroBusca *)arg;
+    long long *arrLocal = dados->array;
+    int nLocal = dados -> n;
+    long long X = dados->value;
+
+    int mid;
+    int low = 0;
+    int high = nLocal;
+
+    while (low < high) {
+        mid = low + (high - low) / 2;
+
+        if (X <= arrLocal[mid]) {
+            high = mid;
+        }
+        else {
+            low = mid + 1;
+        }
+    }
+
+    if(low < nLocal && arrLocal[low] < X)
+       low++;
+    
+    //armazena o resultado local no vetor
+    // esse index é para se uma thread terminar antes nao pegar a posicao de outra no vetor
+    dados->Pos[dados->index] = low;
+    //pthread_barrier_wait(&mybarrier);
+    printf("posicao %ls, para o valor %lld\n", dados->Pos, dados->value);
+}
 
 int bsearch_lower_bound_A(long long arr[], int N, long long X)
 {
@@ -50,9 +88,10 @@ int main() {
 
     long long Q[] = { 3, 8, 9 };
     int nQ = 3;
-    pthread_t ids[nQ];
+    pthread_t thread_[nQ];
     int short_ids[nQ];
 
+    ParametroBusca parametroBusca[nQ];
     int *Pos = (int *)malloc(nQ * sizeof(int)); // Aloca memória para o array Pos
 
     // Pos = bsearch_lower_bound_B(Input, n, Q, nQ, Pos);
@@ -71,22 +110,27 @@ int main() {
     // }
 
     srand(time(NULL));
-    pthread_barrier_init(&mybarrier, NULL, nQ + 1);
+    //pthread_barrier_init(&mybarrier, NULL, nQ + 1);
 
     for (int i=0; i < nQ; i++) {
+        parametroBusca[i].array = Input;
+        parametroBusca[i].n = n;
+        parametroBusca[i].value = Q[i];
+        parametroBusca[i].Pos = Pos;
         short_ids[i] = i;
-        pthread_create(&ids[i], NULL, bsearch_lower_bound_A, &short_ids[i]);
-    }
 
-    pthread_barrier_wait(&mybarrier);
+        printf("Thread %d: posicao em %lld\n", short_ids[i], parametroBusca[i].value);
+        pthread_create(&thread_[i], NULL, bsearch_lower_bound_Parallel, &parametroBusca[i]);
+    }
+    //pthread_barrier_wait(&mybarrier);
 
     printf("main() is going!\n");
 
-    for (int i=0; i < THREAD_COUNT; i++) {
-        pthread_join(ids[i], NULL);
+    for (int i=0; i < nQ; i++) {
+        pthread_join(thread_[i], NULL);
     }
 
-    pthread_barrier_destroy(&mybarrier);
+    //pthread_barrier_destroy(&mybarrier);
 
     free(Pos); // Libera a memória alocada para Pos
 
